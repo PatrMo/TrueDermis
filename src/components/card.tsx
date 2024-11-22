adfc04d679c032d7450b028f859df762ab049295
PatrMo <molka.patrick@gmail.com>
2024-11-22 12:01:39 -0500

    layed some groundwork for frontend

---
+++ src/components/card.tsx
@@ -0,0 +1,20 @@
+/*
+* TODO: 
+* figure it out, i havent looked into this component enough yet
+*
+* 
+* 
+*/
+
+import React from 'react'
+
+
+export const Card: React.FC = async () => {
+    return (
+        <nav>
+            
+        </nav>
+    )
+}
+
+export default Card
---
+++ src/components/navbar.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+/*
+* TODO:
+* create a navbar compnonet
+* do whatever you want here, it will be included in the webpage
+* I already exported this function to layout.tsx
+*/
+
+export const Navbar: React.FC = async () => {
+    return (
+        <nav>
+            
+        </nav>
+    )
+}
+
+export default Navbar
